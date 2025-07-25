0a6838d2f9cebb133009ed14d7065a640bf9940c
abhiabhi94 <13880786+abhiabhi94@users.noreply.github.com>
2025-07-25 23:09:56 +0530

    Add support for Django 4.2-5.2 and Python 3.10-3.13
    - update supported Django versions from 2.2-3.2 to 4.2-5.2
    - update supported Python versions from 3.6-3.10 to 3.10-3.13
    - drop support for Django and Python versions that have reached
      end of life.
    - remove deprecated Django features and compatibility code

--- django_cute_status/conf/__init__.py
+++ django_cute_status/conf/__init__.py
@@ -10,12 +10,12 @@ class LazySettings(LazyObject):
         self._wrapped = Settings(app_settings, django_settings)
 
 
+_django_version = django.VERSION
+
 DEPRECATED_SETTINGS = {
-    'USE_TZ' if django.VERSION > (4, 0) else None,
-    'PASSWORD_RESET_TIMEOUT_DAYS' if django.VERSION > (3, 0) else None,
-    'DEFAULT_CONTENT_TYPE' if django.VERSION > (2, 2) else None,
-    'FILE_CHARSET' if django.VERSION > (2, 2) else None,
-    'USE_L10N' if django.VERSION > (4, 0) else None,
+    'DEFAULT_FILE_STORAGE' if (4, 2) <= _django_version < (5, 1) else None,
+    'STATICFILES_STORAGE' if (4, 2) <= _django_version < (5, 1) else None,
+    'USE_L10N' if (4, 0) <= _django_version < (5, 0) else None,
 }
 
 
@@ -25,7 +25,7 @@ class Settings:
             setattr(self, attr, getattr(item, attr))
             for item in args
             for attr in dir(item)
-            if attr == attr.upper() and attr.upper() not in DEPRECATED_SETTINGS
+            if attr == attr.upper() and attr not in DEPRECATED_SETTINGS
         ]
 
 
